8e00754cfb74e555751b70859e5a78cc4bebc9b5
Palas Paunikar <ppaunikar@delaplex.com>
2021-05-26 10:19:39 +0530

    added new changes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
 
 namespace DataIngestion.TestAssignment
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+    class Program
+    {
+        /// <summary>
+        /// This is the starting point.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static async Task Main(string[] args)
+        {            
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetAsync("https://localhost:44381/api/LinkFireReadAllData/ReadDataRange");
+
+            }
+
+        }
+    }
 }
